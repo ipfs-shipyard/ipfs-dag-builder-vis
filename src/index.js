f8a2e53682eba189f1871b7ea79db5f1b7878ad4
Marcin Rataj <lidel@lidel.org>
2022-08-30 15:08:29 +0200

    chore: dag.ipfs.tech
    https://github.com/protocol/bifrost-infra/issues/2018#issuecomment-1220776463

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+// normalize to .tech tld
+// https://github.com/protocol/bifrost-infra/issues/2018
+const { href } = window.location
+if (href.includes('dag.ipfs.io')) {
+  window.location.replace(href.replace('dag.ipfs.io', 'dag.ipfs.tech'))
+}
+if (href.includes('dag-ipfs-io')) {
+  window.location.replace(href.replace('dag-ipfs-io', 'dag-ipfs-tech'))
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
