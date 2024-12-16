69144a6c2362c6c09068d12a73f7207a972cf485
Daniel Norman <1992255+2color@users.noreply.github.com>
2024-12-16 11:30:14 +0100

    fix: color

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,8 @@ export default function Header () {
       <a href='https://ipfs.tech' title='home' className='w-50'>
         <img src={Logo} style={{ height: 50 }} />
       </a>
-      <h1 className='w-50 ma0 tr f3 fw2 montserrat aqua'>
-        <a href='https://github.com/ipfs-shipyard/ipfs-dag-builder-vis'>DAG builder</a>
+      <h1 className='w-50 ma0 tr f3 fw2 montserrat'>
+        <a className="aqua" href='https://github.com/ipfs-shipyard/ipfs-dag-builder-vis'>DAG builder</a>
       </h1>
     </header>
   )
